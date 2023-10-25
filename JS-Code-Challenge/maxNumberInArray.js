47c308a20e271ddb4a89216b2f7da6f12ee01ca5
Bittu <bittu1040@gmail.com>
2023-10-26 01:38:07 +0530

    Added js coding practise list

---
+++ JS-Code-Challenge/maxNumberInArray.js
@@ -0,0 +1,3 @@
+let arr1= [1,2,4,5,6,3];
+const maxNumber=Math.max(...arr1);
+console.log("max number",maxNumber);
---
+++ JS-Code-Challenge/reverseArray.js
@@ -0,0 +1,4 @@
+let arr= [1,2,3,4];
+let reversedArr=arr.reverse();
+// console.log("reversed array is",reversedArr);
+// console.log("original array is also modified", arr);
