f1d083388593e51a8fcc706bccad4ddca602b0a8
Bittu <bittu1040@gmail.com>
2023-12-18 13:25:36 +0530

    added node and hoisting

---
+++ JS-Topics/Hoisting.js
@@ -0,0 +1,20 @@
+// Hoisting
+
+/* Variables and function declarations are moved to the top of their scope before code execution.
+   Variables can be declared after they are used.
+   Remember : only declaration is hoisted not initialization.
+*/
+
+// we can access function before it is declared. 
+test()
+function test(){
+    console.log("bbb")
+}
+
+test()
+
+console.log(a);
+var a;
+
+
+// question 1:
