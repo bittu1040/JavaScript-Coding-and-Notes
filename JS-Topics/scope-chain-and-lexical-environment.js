9ff6a0f18945eb361e7467fe8d7cdfdb05d4c70a
Bittu <bittu1040@gmail.com>
2023-12-21 22:09:13 +0530

    scope chain and lexical environment example

---
+++ JS-Topics/scope-chain-and-lexical-environment.js
@@ -0,0 +1,32 @@
+// scope chain & lexical environment
+// variable having access in its parent scope
+
+function outer() {
+    let username = "Bittu";
+    // console.log(secret)
+    function inner() {
+        let secret = "Bittu123"
+        console.log("Inner", username);
+        console.log(secret)
+    }
+    function innerTwo() {
+        console.log("innerTwo", username);
+        // console.log(secret)
+    }
+    inner();
+    innerTwo();
+}
+
+outer();
+
+// console.log(x);
+
+if (Math.random() > 0.5) {
+    let x = 1;
+} else {
+    let x = 2;
+}
+
+// console.log(x);
+
+
