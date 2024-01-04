10f8c138e1c83d35543cdbde2f6c58113a82620d
Bittu <bittu1040@gmail.com>
2024-01-04 11:57:33 +0530

    swap string first and last

--- JS-Code-Challenge/swapFirstAndLastcharacter.js
+++ JS-Code-Challenge/swapFirstAndLastcharacter.js
@@ -11,3 +11,16 @@ function swapFirstAndLast(str){
 }
 
 console.log(swapFirstAndLast("hello"))
+
+
+function swap(str){
+    if(str.length<2){
+        return str;
+    }
+    return str.charAt(str.length-1) + str.substring(1, str.length-1) +  str.charAt(0);
+}
+
+console.log(swap("Bittu"))
+console.log(swap("B"))
+
+
