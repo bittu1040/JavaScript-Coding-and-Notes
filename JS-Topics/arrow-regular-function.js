0c0ad2b807f558b051203bff5e5f6b71ffce0c12
Bittu <bittu1040@gmail.com>
2024-01-21 01:40:53 +0530

    arrow function

---
+++ JS-Topics/arrow-regular-function.js
@@ -0,0 +1,93 @@
+// this refers to the object that is currently calling the function in case of regular function
+// arrow function: this refers to the lexical parent of the function
+
+
+
+
+// console.log(this);
+
+// Example 1:
+/*
+var age= 20;
+var city="blr"
+function displayAge(){
+    console.log(this.age);
+    console.log("city:", this.city);
+    console.log(this)
+}
+displayAge();   
+*/ 
+// It is same as window.displayAge();
+// here this refers to window because window is calling displayAge function
+
+// example 2
+
+/*
+var age=20;
+var person= {
+    age: 25,
+    city: "Blr", 
+    displayAge: function(){
+        console.log(this)
+        console.log(this.age);
+        console.log("city:", this.city);
+    },
+    nestedObject: {
+        age: 40,
+        city: "Pune",  
+        displayAge: function(){
+            console.log(this)
+            console.log(this.age);
+        }
+    },
+    arrowMethod: {
+        age: 50,
+        city: "Mumbai",  
+        displayAge: () => {
+            console.log(this)
+            console.log(this.age);
+        }
+    }
+
+}
+
+person.displayAge();
+person.nestedObject.displayAge();
+person.arrowMethod.displayAge();
+
+*/
+
+
+// example 3
+
+
+var person = {
+    age: 28,
+    greet1: function(){
+        console.log("greet 1",this);
+        console.log("greet 1",this.age);
+        function greet2(){
+            console.log("greet 2",this);
+            console.log("greet 2",this.age);
+        }
+        const greet3=()=>{
+            console.log("greet 3",this);
+            console.log("greet 3",this.age);
+        }
+        greet2();
+        greet3();
+    }
+}
+
+let person1= {
+    age: 50,
+}
+
+// person.greet1.call(person1);
+person.greet1();
+
+
+
+
+
+
