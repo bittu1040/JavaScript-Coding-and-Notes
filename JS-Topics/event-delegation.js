45ee5f8d55e03deb802e111986a32c960775dac0
Bittu <bittu1040@gmail.com>
2024-01-25 23:26:49 +0530

    event delegation

---
+++ JS-Topics/event-delegation.js
@@ -0,0 +1,3 @@
+// event delegation is a design pattern, single parent element is used to manage and handle events for multiple child elements. 
+// event propagation is the phase including event capturing and event bubbling. 
+// Event capturing and event bubbling are two distinct phases of event propagation.
