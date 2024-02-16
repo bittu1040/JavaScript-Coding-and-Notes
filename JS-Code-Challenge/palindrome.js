8b2aa392dd12740957d0d795bbc5d2571e3048e6
Bittu <bittu1040@gmail.com>
2024-02-17 01:40:19 +0530

    palindrome check

--- JS-Code-Challenge/palindrome.js
+++ JS-Code-Challenge/palindrome.js
@@ -1,16 +1,29 @@
 // palindrome number
 
-function palindrome(num){
+function palindrome(num) {
 
 
-   if(num.toString().split("").reverse().join("")== num){
-    return true;
+   if (num.toString().split("").reverse().join("") == num) {
+      return true;
    }
    else {
-    return false;
+      return false;
    }
 
 }
 
 console.log(palindrome(121));
+
+
+function PalindromeNumberOrString(input) {
+   if (input.toString().split("").reverse().join("") == input) {
+      return true;
+   }
+   else {
+      return false;
+   }
+}
+
+console.log(PalindromeNumberOrString("malayalam"));
+console.log(PalindromeNumberOrString(121));
 
