3fbc32ccd18d242e286f286bdce3223a5714d66a
Bittu <bittu1040@gmail.com>
2024-03-06 01:26:49 +0530

    coercion

---
+++ JS-Topics/coercion.js
@@ -0,0 +1,6 @@
+// coercion
+
+// Coercion is an automatic type conversion that occurs in JavaScript when you want to perform certain operations.
+// Type conversion can either be implicit (automatically done during code execution) or explicit (done by you the developer).
+
+const sum = 35 + "hello"
