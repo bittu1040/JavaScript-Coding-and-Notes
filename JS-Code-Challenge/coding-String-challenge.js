407e8a1e97fed0f63751a87a6ed402ddf2d330f2
Bittu <bittu1040@gmail.com>
2024-03-10 23:56:48 +0530

    Slice and splice example

--- JS-Code-Challenge/coding-String-challenge.js
+++ JS-Code-Challenge/coding-String-challenge.js
@@ -70,6 +70,23 @@ console.log(findMaxValue(obj))
 // 6  slice and splice
 /*
     slice : 
-    arr.slice()
+    array.slice(startIndex, endIndex);    both index are optional and endIndex is not included
+    It does not modify original array
+    It return a shallow copy of a portion of an array into a new array object
+
+    splice :
+    This method is used for making modifications to the original array by adding, removing, or replacing elements
+    array.splice(startIndex, deleteCount, item1, item2, ...);
 
 */
+
+
+let arr1= [1,2,3,4,5];
+let arr2= arr1.slice(2,3)
+console.log(arr1)
+console.log(arr2)
+
+let arr3= [1,2,3,4,5,6]
+let arr4= arr3.splice(1,1,10,20)
+console.log(arr3)
+console.log(arr4)
