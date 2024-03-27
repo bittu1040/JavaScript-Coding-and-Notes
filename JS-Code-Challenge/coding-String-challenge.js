dc2ac5e6ed405c0d8f9376e20020a798833258ad
Bittu <bittu1040@gmail.com>
2024-03-27 21:37:01 +0530

    count character

--- JS-Code-Challenge/coding-String-challenge.js
+++ JS-Code-Challenge/coding-String-challenge.js
@@ -41,16 +41,16 @@ function findNumberOfChar(str) {
 // 3.   find maximum value from an object
 let obj = { t: 4, h: 3, i: 4, s: 3, j: 1 };
 
-function findMaxValue(obj){
-    let max=0;
-    for(let data in obj){
+function findMaxValue(obj) {
+    let max = 0;
+    for (let data in obj) {
         // console.log(obj[data])
-        if(obj[data]>max){
-            max= obj[data];
+        if (obj[data] > max) {
+            max = obj[data];
         }
     }
-    for(let data in obj){
-        if(obj[data]===max){
+    for (let data in obj) {
+        if (obj[data] === max) {
             return data
         }
     }
@@ -81,36 +81,51 @@ function findMaxValue(obj){
 */
 
 
-let arr1= [1,2,3,4,5];
-let arr2= arr1.slice(2,3)
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = arr1.slice(2, 3)
 // console.log(arr1) // [ 1, 2, 3, 4, 5 ]
 // console.log(arr2) // [ 3 ]
 
-let arr3= [1,2,3,4,5,6]
-let arr4= arr3.splice(1,1,10,20)
+let arr3 = [1, 2, 3, 4, 5, 6]
+let arr4 = arr3.splice(1, 1, 10, 20)
 // console.log(arr3)
 // console.log(arr4)
 
 
 // 7. Create your own string method: repeatify(3)- input string will be repeated three times
-      // Ex- console.log("hello".repeatString(3)); // HelloHelloHello
+// Ex- console.log("hello".repeatString(3)); // HelloHelloHello
 
-String.prototype.repeatString= function(times){
-    let str="";
-    for(let i=0; i<times; i++){
-        str= str+this;  // here this= "Hello"
+String.prototype.repeatString = function (times) {
+    let str = "";
+    for (let i = 0; i < times; i++) {
+        str = str + this;  // here this= "Hello"
     }
     return str;
 }
 // console.log("Hello".repeatString(3))  // HelloHelloHello
-  
+
 
 
 // 8. Write a program to mask card number 
 
-const cardNumber= "1234234534564567";
-const len= cardNumber.length;
-const last4= cardNumber.slice(len-4)
-const remaining= cardNumber.slice(0,len-4);
-const masked= "*".repeat(remaining.length)+ last4
+const cardNumber = "1234234534564567";
+const len = cardNumber.length;
+const last4 = cardNumber.slice(len - 4)
+const remaining = cardNumber.slice(0, len - 4);
+const masked = "*".repeat(remaining.length) + last4
 console.log(masked)   // ***************4567
+
+
+// 9. Write a JavaScript program to return count of each character in a string.
+
+function countCharacter(str) {
+    let charCount = {};
+    for (let s of str) {
+        if(s!==" ") {
+            charCount[s]= (charCount[s] || 0) + 1
+        }
+    }
+    return charCount
+}
+
+console.log("cc",countCharacter("my name is bittu"));
