c4ee00b152ebd02e93048422b449a50dd9ca0a34
Bittu <bittu1040@gmail.com>
2024-05-07 17:22:21 +0530

    frequency of key in object

--- JS-Code-Challenge/coding-object-problem.js
+++ JS-Code-Challenge/coding-object-problem.js
@@ -5,5 +5,30 @@
 // 3. Object property sorting
 // 4. Object property renaming
 // 5. Object Property aggregation
+// 6. Object property frequency
 
-1. 
+
+
+//1.  Write a JavaScript program that takes an array of objects and returns an object containing the frequency of each property across all objects.
+
+function getPropertyFrequency(arr){
+    const frequency={}
+    arr.forEach((obj)=>{
+        for(const key in obj){
+            if(frequency[key]){
+                frequency[key]++
+            }else{
+                frequency[key]=1
+            }
+        }
+    })
+    return frequency;
+}
+
+const data= [
+    {a: 1, b: 2},
+    {b: 3, c: 4},
+    {a: 5, b: 6}
+]
+
+console.log(getPropertyFrequency(data))  // { a: 2, b: 3, c: 1 }
