7bb855c09b6526c2ff83acd369bc31cb5356d7b0
Bittu <bittu1040@gmail.com>
2024-05-17 12:36:17 +0530

    find duplicate

--- JS-Code-Challenge/coding-practice-1.js
+++ JS-Code-Challenge/coding-practice-1.js
@@ -612,3 +612,25 @@ function findCommonProperty(obj1, obj2){
 
 console.log(findCommonProperty(obj1, obj2));
 
+// 26. Write a JavaScript program to find duplicate number from an array.
+
+function findDuplicates(arr) {
+    let res = {};
+    let duplicate=[];
+    for (let i = 0; i < arr.length; i++) {
+        if(!res[arr[i]]){
+            res[arr[i]] = 1;
+        }else{
+            res[arr[i]]++;
+        }
+    }
+    for(let data in res){
+        if(res[data]>1){
+            duplicate.push(parseInt(data));
+        }
+    }
+
+    return duplicate
+}
+
+console.log(findDuplicates([1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 8, 9, 9])); // [3, 5, 8, 9]
