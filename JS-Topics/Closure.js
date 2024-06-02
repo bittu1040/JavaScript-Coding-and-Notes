e01dc4bac0f8e6fa915c27df226759d175eb869b
Bittu <bittu1040@gmail.com>
2024-06-02 13:59:51 +0530

    closure

--- JS-Topics/Closure.js
+++ JS-Topics/Closure.js
@@ -70,3 +70,36 @@ console.log(test1(2)(3))
 
 
 
+function createPerson(name,age){
+    //private variables
+    let privateName = name;
+    let privateAge = age;
+ 
+    //private method
+    function isAdult(){
+        return age>=18;
+      }
+ 
+     //public methods with closure access to private variables and private methods
+     return {
+         getName : function(){
+             return privateName;
+         },
+         getAge : function(){
+             return privateAge
+         },
+         setName : function(name){
+             privateName = name
+         },
+         setAge : function(age){
+             privateAge = age;
+         },
+         isAdult : isAdult
+     }
+   }
+ 
+   let person = createPerson('Bittu',21)
+   console.log(person.privateAge) //undefined as not allowed to access private variable
+   console.log(person.getName());  // Bittu
+   console.log(person.getAge());   // 21
+   console.log(person.isAdult());  // true
