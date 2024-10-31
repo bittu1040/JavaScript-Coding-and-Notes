9c9b6906c15798d8ca3f04939ea7ce98088c79f3
Bittu <bittu1040@gmail.com>
2024-10-31 13:55:29 +0530

    setInterval

---
+++ JS-Topics/setInterval.js
@@ -0,0 +1,25 @@
+
+/*
+📌 setInterval runs a function repeatedly at fixed time intervals
+📌 Use clearInterval(intervalId) to stop it, avoiding infinite loops
+📌 Asynchronous behavior: It runs independently of previous calls, which can lead to overlapping if tasks take longer than the interval.
+*/
+
+
+
+// print even numbers from an array at intervals of 2 seconds
+let arr = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+
+function printEven(arr) {
+  let index = 0
+  let intervalId = setInterval(() => {
+    if (index >= arr.length) {
+      clearInterval(intervalId)
+    } else if (arr[index] % 2 === 0) {
+      console.log(arr[index])
+    }
+    index++
+  }, 2000)
+}
+
+printEven(arr);   // 12 14 16 18 20 22
