cbbe80cdbe96babe4d9a785a731717195b36af89
Bittu <bittu1040@gmail.com>
2024-11-01 20:46:19 +0530

    transform array of objects ; reduce

--- JS-Code-Challenge/Coding-practice/array-object-manupulation.js
+++ JS-Code-Challenge/Coding-practice/array-object-manupulation.js
@@ -16,24 +16,61 @@ function mergeObjects(obj1, obj2) {
     return obj1;
 }
 
-console.log(mergeObjects(obj1, obj2));
+// console.log(mergeObjects(obj1, obj2));
 
 // way 2
 function mergeObjects1(obj1, obj2) {
     return {...obj1, ...obj2}
 }
 
-console.log(mergeObjects1(obj1, obj2));
+// console.log(mergeObjects1(obj1, obj2));
 
 // way 3
 function mergeObjects3(obj1, obj2) {
     return Object.assign({}, obj1, obj2)
 }
 
-console.log(mergeObjects3(obj1, obj2));
+// console.log(mergeObjects3(obj1, obj2));
 
 
 
 
+
+
+// 2 Transform Array of Objects: 
+
+/*
+     Given an array of objects, return an object where keys are derived from one of the object properties, 
+     and values are arrays of objects.
+     Example: Convert [{ id: 1, name: 'A' }, { id: 2, name: 'B' }] into { 1: { name: 'A' }, 2: { name: 'B' } }
+*/
+
+let arr1= [{ id: 1, name: 'A' }, { id: 2, name: 'B' }];
+
+// way 1
+function transformArrayOfObject(arr){
+    let obj={}
+    for (const element of arr) {
+        obj[element.id]= {name:element.name}
+    }
+    return obj;
+}
+
+// console.log(transformArrayOfObject(arr1));
+
+// way 2 using reduce 
+
+function transformArrayOfObject1(arr){
+    return arr.reduce((acc,item)=>{
+        acc[item.id]= {name: item.name}
+        return acc;
+    }, {})
+}
+
+// console.log(transformArrayOfObject1(arr1));
+
+
+
+// 3. 
 
 
