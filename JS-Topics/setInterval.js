62ecc2e9945b06582ba87bb8357be8ece8a02e12
Bittu Kumar <32195733+bittu1040@users.noreply.github.com>
2025-01-15 02:27:06 +0530

    Update setInterval.js

--- JS-Topics/setInterval.js
+++ JS-Topics/setInterval.js
@@ -8,6 +8,21 @@
 
 // print 1 to 10 in each 1 second using setInterval
 
+function printNumbers(){
+	let i=1;
+	let intervalId= setInterval(()=>{
+  	console.log(i);
+    i++;
+    if(i>10){
+    	clearInterval(intervalId)
+    }
+  }, 1000)
+}
+
+printNumbers();
+
+
+
 
 // print even numbers from an array at intervals of 2 seconds
 let arr = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
